dd14dd7fb6a9f787adfdc6e515c4b85844ed37f3
Muramatsu8618 <muramatsu8618@gmail.com>
2018-04-10 21:57:20 +0900

    third commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,31 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: '登録が完了しました'
+    else
+      flash.now[:alert] = "登録に失敗しました"
+      render :new
+    end
+  end
+
+    private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   # require "mysql2"
-  # require "active_recode"
+  # require "ActiveRecode"
   #
   # Dir[__dir__ + '/models/*.rb'].each(&method(:require))
   #
   # ActiveRecode::Base.establish_connection(
   #   :adapter => 'mysql2',
-  #   :database => __dir__ + '/db/'
+  #   :database => __dir__ + '/db/necokaburi_development'
   # )
   #
   # def create_account_table(code)
@@ -41,3 +57,4 @@ class UsersController < ApplicationController
   #   model_path = __dir__ + "/models/" + account
   #   require_relative model_path
   # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  get 'users/new'
+  # get 'users/new'
+  resources 'users'
 end
