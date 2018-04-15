66897d665d32769d39e42e614a95a3946137468e
Muramatsu8618 <muramatsu8618@gmail.com>
2018-04-15 18:48:34 +0900

    アカウント設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411111748) do
+ActiveRecord::Schema.define(version: 20180415093622) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20180411111748) do
     t.string "password"
     t.datetime "birthday"
     t.string "gender"
-    t.integer "phone"
+    t.string "phone"
     t.string "address"
     t.text "comment"
     t.integer "users_id"
